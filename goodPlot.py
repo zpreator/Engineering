8f6bd4c2d8e7aadecc8efd58aaad60e8f3354c28
zach preator <zachary.preator@gmail.com>
2020-02-13 22:55:34 -0700

    uh

--- goodPlot.py
+++ goodPlot.py
@@ -66,36 +66,36 @@ def main():
 	display(delPs, velocities, Cps, normalized)
 	
 main()
-# Change plt font family and text size
-plt.rc('font', family='serif', size=10)
+# # Change plt font family and text size
+# plt.rc('font', family='serif', size=10)
 
-# Example data
-#t = np.linspace(0,1,num=100,endpoint=True)
-#s = np.cos(4 * np.pi * t) + 2
+# # Example data
+# #t = np.linspace(0,1,num=100,endpoint=True)
+# #s = np.cos(4 * np.pi * t) + 2
 
-# Example descreate data
-tData = np.arange(-3.5, 4.0, .5)
-sData = delPs
+# # Example descreate data
+# tData = np.arange(-3.5, 4.0, .5)
+# sData = delPs
 
-#tData = np.linspace(0,1,num=20,endpoint=True)
-#sData = np.cos(4 * np.pi * tData) + 2+np.random.random(len(tData))/10
-#uData = np.random.random(len(tData))/10
+# #tData = np.linspace(0,1,num=20,endpoint=True)
+# #sData = np.cos(4 * np.pi * tData) + 2+np.random.random(len(tData))/10
+# #uData = np.random.random(len(tData))/10
 
-# Make the plot
-#plt.figure(figsize=(5,3)) #set the figuer size (width,height)(inches)
-#plt.plot(t,s,'-.',label='Modeled values')
-plt.errorbar(tData,sData,fmt='o',capsize=5,label='Mesured Values')
+# # Make the plot
+# #plt.figure(figsize=(5,3)) #set the figuer size (width,height)(inches)
+# #plt.plot(t,s,'-.',label='Modeled values')
+# plt.errorbar(tData,sData,fmt='o',capsize=5,label='Mesured Values')
 
-plt.xlabel('Time (s)')
-plt.ylabel('Voltage (mV)')
-#plt.xlabel(r'Time ($s$)') #the r'...' alows for LaTeX math input
-# plt.ylabel(r'Voltage ($mV$)')
-plt.legend(loc='lower right')
-#plt.grid()
-plt.tight_layout()
-#plt.show()
-#plt.savefig('goodPlot.pdf') # save the image as a vecortized pdf
-plt.show()
+# plt.xlabel('Time (s)')
+# plt.ylabel('Voltage (mV)')
+# #plt.xlabel(r'Time ($s$)') #the r'...' alows for LaTeX math input
+# # plt.ylabel(r'Voltage ($mV$)')
+# plt.legend(loc='lower right')
+# #plt.grid()
+# plt.tight_layout()
+# #plt.show()
+# #plt.savefig('goodPlot.pdf') # save the image as a vecortized pdf
+# plt.show()
 
 
 
