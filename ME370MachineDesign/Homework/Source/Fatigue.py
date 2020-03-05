214795269dba122ed39f8a2c2d96ac71bfc76c05
zach preator <zachary.preator@gmail.com>
2020-03-04 23:24:54 -0700

    HW20

---
+++ ME370MachineDesign/Homework/Source/Fatigue.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def Goodman(SigmaA, SigmaM, Se, Sut):
+    return 1/(SigmaA/Se+SigmaM/Sut)
+
+def Gerber(SigmaA, SigmaM, Se, Sut):
+    return .5*(Sut/SigmaM)**2*(SigmaA/Se)*(-1+np.sqrt(1+(2*SigmaM*Se/(Sut*SigmaA))**2))
+
+def CyclesA(f, Sut, Se):
+    return (f*Sut)**2/Se
+
+def CyclesB(f, Sut, Se):
+    return -1/3*np.log10(f*Sut/Se)
+
+def Cycles(sigmaRev, f, Sut, Se):
+    a = CyclesA(f, Sut, Se)
+    b = CyclesB(f, Sut, Se)
+    N = (sigmaRev/a)**(1/b)
+    return N
